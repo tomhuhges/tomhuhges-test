86a396c5004bdd48461535fb7566591b4a3d315a
tomhuhges <merzbear@gmail.com>
2016-11-24 22:18:51 +0000

    add info

--- src/index.js
+++ src/index.js
@@ -2,22 +2,40 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Snow from 'react-snow-effect';
 
-let gif = document.createElement('img');
-gif.setAttribute('src', 'assets/img/computer2.gif');
-gif.style.width = '500px';
-document.body.appendChild(gif);
+let googlefonts = document.createElement('link');
+googlefonts.href = 'https://fonts.googleapis.com/css?family=Source+Code+Pro:200,400|Astloch';
+googlefonts.setAttribute('rel', 'stylesheet');
+document.head.appendChild(googlefonts);
 
-let snow = document.createElement('div');
-snow.id = 'snow';
-document.body.appendChild(snow);
+let main = document.createElement('div');
+main.id = 'main';
+document.body.appendChild(main);
 
 let Site = React.createClass({
 	render: function () {
-		return <Snow />;
+		return (
+			<div>
+				<div id="snow"><Snow /></div>
+				<div class="container">
+					<div id="title-box">
+						<h1>Tom Hughes</h1>
+						<h2>javascript developer</h2>
+						<p><b>currently:</b> freelance (aka <a>hire me</a>)</p>
+						<p><b>previously:</b> lead developer and designer at springwise</p>
+						<p>hiya, my name is tom hughes and i'm a web developer based in london, uk. i specialize in javascript and like using new technologies like react and es6. in fact, this website is built using both.</p>
+						<p>as well as writing maintainable code, i'm pretty good at design and also make 3d animations and sometimes comics in my spare time.</p>
+						<a>code</a>
+						<a>art</a>
+						<a>writing</a>
+						<p>ok@tomhuhges.com +44 772 5577 327</p>
+					</div>
+				</div>
+			</div>
+			);
 	}
 });
 
 ReactDOM.render(
 	<Site />,
-	document.getElementById('snow')
+	document.getElementById('main')
 );
