374db07ec51854ff8feca8e62eec8cf5e4aaf405
tomhuhges <merzbear@gmail.com>
2016-11-25 16:43:11 +0000

    make css load async

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 
 let googlefonts = document.createElement('link');
 googlefonts.href = 'https://fonts.googleapis.com/css?family=Source+Code+Pro:200,400|Astloch';
-googlefonts.setAttribute('rel', 'stylesheet');
+googlefonts.setAttribute('rel', 'preload');
+googlefonts.setAttribute('as', 'style');
+googlefonts.setAttribute('onload', 'this.rel="stylesheet"');
 document.head.appendChild(googlefonts);
 
 let app = document.createElement('div');
@@ -18,19 +20,19 @@ let Site = React.createClass({
 					<div id="title-box">
 						<h1>Tom Hughes</h1>
 						<h2>javascript developer</h2>
+						<a href="https://twitter.com/tomhuhges">twitter</a> <a href="https://github.com/tomhuhges">github</a>
 						<p><b>currently:</b> freelance (aka <a>hire me</a>)</p>
 						<p><b>previously:</b> lead developer and designer at springwise</p>
-						<p>hiya, my name is tom hughes and i'm a web developer based in london, uk. i specialize in javascript and like using new technologies like&nbsp;
+						<p>hiya, my name is tom hughes and i'm a web developer based in london, uk. i specialize in javascript and like using new technologies like <b>
 						<span style={{color: 'red'}}>r</span>
 						<span style={{color: 'yellow'}}>e</span>
 						<span style={{color: 'lime'}}>a</span>
 						<span style={{color: 'magenta'}}>c</span>
-						<span style={{color: 'orange'}}>t</span> and&nbsp;
+						<span style={{color: 'orange'}}>t</span></b> and <b>
 						<span style={{color: 'cyan'}}>e</span>
 						<span style={{color: 'pink'}}>s</span>
-						<span style={{color: 'turquoise'}}>6</span>. in fact, this website is built using both.</p>
+						<span style={{color: 'turquoise'}}>6</span></b>. in fact, this website is built using both.</p>
 						<p>as well as writing maintainable code, i'm pretty good at design and also make 3d animations in my spare time.</p>
-						<a href="https://twitter.com/tomhuhges">twitter</a> <a href="https://github.com/tomhuhges">github</a>
 						<p>ok@tomhuhges.com +44 772 5577 327</p>
 					</div>
 					<img id="computer" src="assets/img/computer5.gif" />
