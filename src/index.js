137221d256a694f4bcdce0e53f2742dce17ab1e6
tomhuhges <merzbear@gmail.com>
2016-11-25 02:02:14 +0000

    add gif

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ let Site = React.createClass({
 						<a>writing</a>
 						<p>ok@tomhuhges.com +44 772 5577 327</p>
 					</div>
-					<img id="computer" src="assets/img/computer3.gif" />
+					<img id="computer" src="assets/img/computer4.gif" />
 				</div>
 			</div>
 			);
