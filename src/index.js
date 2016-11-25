57318573c26bd921be0b3888a4eae878eb4dbe0c
tomhuhges <merzbear@gmail.com>
2016-11-25 02:10:23 +0000

    add gif

--- src/index.js
+++ src/index.js
@@ -22,11 +22,17 @@ let Site = React.createClass({
 						<h2>javascript developer</h2>
 						<p><b>currently:</b> freelance (aka <a>hire me</a>)</p>
 						<p><b>previously:</b> lead developer and designer at springwise</p>
-						<p>hiya, my name is tom hughes and i'm a web developer based in london, uk. i specialize in javascript and like using new technologies like react and es6. in fact, this website is built using both.</p>
+						<p>hiya, my name is tom hughes and i'm a web developer based in london, uk. i specialize in javascript and like using new technologies like 
+						<span style="color:red">r</span>
+						<span style="color:yellow">e</span>
+						<span style="color:lime">a</span>
+						<span style="color:magenta">c</span>
+						<span style="color:orange">t</span> and 
+						<span style="color:cyan">e</span>
+						<span style="color:pink">s</span>
+						<span style="color:turquoise">6</span>. in fact, this website is built using both.</p>
 						<p>as well as writing maintainable code, i'm pretty good at design and also make 3d animations and sometimes comics in my spare time.</p>
-						<a>code</a>
-						<a>art</a>
-						<a>writing</a>
+						<a>code</a> <a>art</a> <a>writing</a>
 						<p>ok@tomhuhges.com +44 772 5577 327</p>
 					</div>
 					<img id="computer" src="assets/img/computer4.gif" />
