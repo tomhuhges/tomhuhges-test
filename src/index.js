51c36f499484225a5419dda6686971ed373c995b
tomhuhges <merzbear@gmail.com>
2016-11-26 18:39:35 +0000

    fix font

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 let googlefonts = document.createElement('link');
-googlefonts.href = 'https://fonts.googleapis.com/css?family=Source+Code+Pro:200,400|Astloch';
+googlefonts.href = 'https://fonts.googleapis.com/css?family=Source+Code+Pro:200,400';
 googlefonts.setAttribute('rel', 'stylesheet');
 // googlefonts.setAttribute('rel', 'preload');
 // googlefonts.setAttribute('as', 'style');
