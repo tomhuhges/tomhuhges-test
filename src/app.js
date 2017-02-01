44904e6d339ea49dc46d02fdff55cbf96c1462fb
tomhuhges <merzbear@gmail.com>
2017-02-01 08:06:22 +0000

    add transitions

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import DocumentHead from 'react-helmet'
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import Header from './components/Header'
 
 class App extends React.Component {
@@ -17,10 +18,17 @@ class App extends React.Component {
 						}
 					]}
 				/>
-				<div id="main" className={`page ${pageName} code bg-light-green blue flex justify-center pa4 w-100-l`}>
-					<div className="container w-two-thirds-l mw8-l">
+				<div id="main" className={`page ${pageName} code bg-light-green blue flex justify-center pa4-m w-100`}>
+					<div className="container w-80 w-90-m mw6-m mw8-l">
 						<Header />
-						{page}
+						<ReactCSSTransitionGroup
+							transitionName="example"
+							transitionEnterTimeout={500}
+							transitionLeaveTimeout={300}>
+						{React.cloneElement(page, {
+							key: pageName
+						})}
+						</ReactCSSTransitionGroup>
 					</div>
 				</div>
 			</div>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -9,7 +9,7 @@ class Header extends React.Component {
 		return (
 			<nav className="flex flex-wrap flex-nowrap-l justify-between-l items-center-l">
 				<div className="w-100 w-auto-l">
-					<h1>
+					<h1 className="mt5-l">
 						<Link
 							to="/"
 							id="logo"
@@ -17,30 +17,30 @@ class Header extends React.Component {
 						>tom hughes
 					</Link>
 					</h1>
-					<h2 className="flex justify-center justify-start-l">$ javascript developer</h2>
+					<h2 className="flex justify-center justify-start-l">js engineer<span className="astloch">&nbsp;&amp;&nbsp;</span>designer</h2>
+					<ul className="social-links list pr0 flex items-center justify-around justify-start-l w-100 w-auto-l">
+						<li className="">
+							<a href="https://twitter.com/tomhuhges" className="menu-item" target="_blank">
+								<TwitterIcon className="w2" />
+							</a>
+						</li>
+						<li>
+							<a href="https://github.com/tomhuhges" className="menu-item">
+								<GithubIcon className="w2" />
+							</a>
+						</li>
+						<li>
+							<a href="https://www.linkedin.com/in/tom-hughes-57b12080" className="menu-item">
+								<LinkedinIcon className="w2" />
+							</a>
+						</li>
+					</ul>
 				</div>
-				<ul className="nav-links blue list p0 flex justify-between w-100 w-50-l">
-					<li><Link to="/code" className="link blue"># code</Link></li>
-					<li><Link to="/code" className="link blue"># art</Link></li>
-					<li><Link to="/code" className="link blue"># writing</Link></li>
-					<li><Link to="/code" className="link blue"># link dump</Link></li>
-				</ul>
-				<ul className="social-links list p0 flex items-center justify-between w-100 w-auto-l">
-					<li className="">
-						<a href="https://twitter.com/tomhuhges" className="menu-item" target="_blank">
-							<TwitterIcon className="w2" />
-						</a>
-					</li>
-					<li>
-						<a href="https://github.com/tomhuhges" className="menu-item">
-							<GithubIcon className="w2" />
-						</a>
-					</li>
-					<li>
-						<a href="https://www.linkedin.com/in/tom-hughes-57b12080" className="menu-item">
-							<LinkedinIcon className="w2" />
-						</a>
-					</li>
+				<ul className="nav-links blue list pr0 pb3-l flex justify-between w-100 w-50-l">
+					<li><Link to="/code" className="link underline-hover blue"># code</Link></li>
+					<li><Link to="/code" className="link underline-hover blue"># art</Link></li>
+					<li><Link to="/code" className="link underline-hover blue"># writing</Link></li>
+					<li><Link to="/code" className="link underline-hover blue"># link dump</Link></li>
 				</ul>
 			</nav>
 		)
